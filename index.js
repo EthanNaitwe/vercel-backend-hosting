dfbcc6c2b5472b3611bf782fdaaac3fa64b03800
Ethan Naitwe <ethanarinaitwe36@gmail.com>
2025-03-06 21:25:27 +0300

    Revert Test

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const winston = require('winston');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const PORT = 4000;
+
+
+app.use(cors());
+app.use(express.json());
+
+// Morgan middleware for logging HTTP requests
+app.use(morgan('combined'));
+
+// Setting up Winston logger for activity logging
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({ filename: 'activity.log' })
+  ]
+});
+
+// Custom middleware to log activity in the app
+app.use((req, res, next) => {
+  logger.info(`Request to ${req.method} ${req.originalUrl}`);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
